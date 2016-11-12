4b2a47c6a7c87a25cd6e7e0cef1484a6de0acd16
WolfieZero <neil@wolfiezero.com>
2016-11-12 22:31:57 +0000

    added homepage

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Presentations</title>
+        <link rel="stylesheet" href="/fonts/sansation/faces.css">
+        <style>
+            html,
+            body {
+                background: #888;
+                width: 100%;
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                color: #efefef;
+                display: table;
+                font-weight: 100;
+                background-size: cover;
+                font-family: 'SansationLight';
+            }
+
+            ul {
+                padding: 0;
+                list-style: none;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+                background-image: linear-gradient( 45deg, rgba(10,10,10,.9), rgba(20,20,20,.9));
+            }
+
+            .content {
+                width: 90%;
+                margin-left: auto;
+                margin-right: auto;
+                text-align: center;
+                display: inline-block;
+            }
+
+            .content > * {
+                margin-bottom: 1em;
+                text-shadow: 0 0 10px rgba(0,0,0,.5);
+            }
+
+            .title {
+                text-transform: uppercase;
+                font-size: 3em;
+            }
+
+            .presentation-link {
+                color: #a34d88;
+                font-size: 2em;
+                text-decoration: none;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <h1 class="title">Presentations</h1>
+                <ul>
+                    <li><a href="/fluff-and-razor-blades" class="presentation-link">Fluff and Razor Blades</a></li>
+                </ul>
+            </dv>
+        </div>
+    </body>
+</html>
